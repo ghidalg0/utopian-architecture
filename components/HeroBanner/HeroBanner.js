576d73915eeeecdccadf4652cf90b35300d4e079
Guilhem Hidalgo <guilhem.hidalgo@tetraktys-cm.com>
2023-11-11 12:06:32 +0100

    added : clean set up + HeroBanner placeholder + imported font

---
+++ components/HeroBanner/HeroBanner.js
@@ -0,0 +1,9 @@
+const HeroBanner = () => {
+  return (
+    <section className="flex flex-col items-center justify-center h-screen">
+      <h1 className="font-bold text-8xl">Utopian Architecture</h1>
+    </section>
+  );
+};
+
+export default HeroBanner;
